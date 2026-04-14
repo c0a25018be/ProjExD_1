3ea144a9aebb3906702495d8620155db489888cc
c0a25018 <c0a25018be@edu.teu.ac.jp>
2026-04-14 17:21:08 +0900

    演習課題1①

--- pygame_image.py
+++ pygame_image.py
@@ -16,22 +16,22 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
+    move_x=-1
+    move_y=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
 
-        move_x, move_y = -1, 0
-        
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            move_y -= 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            move_y += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            move_x -= 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)    
+            move_x += 2
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
